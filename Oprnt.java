9fcb27447c2c8050f789616b670c5672e1b8ff4b
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-25 12:34:50 +0530

    Add files via upload

---
+++ Oprnt.java
@@ -0,0 +1,10 @@
+package pack1;
+public class Operant1 {
+	public static void main(String[] args) {
+		int x = 1821;
+		System.out.println(x++);
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+		}
+}
