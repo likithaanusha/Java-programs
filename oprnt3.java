9f774c599dbc88db06cf328a24a3e94918d3a541
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-25 12:35:36 +0530

    Add files via upload

---
+++ oprnt3.java
@@ -0,0 +1,14 @@
+package pack1;
+public class Oprnt3 {
+	public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 20;
+        System.out.println(a<b && a<c);
+        System.out.println(a>b && a<c);
+        System.out.println(a<b || a<c);
+        System.out.println(a<b || a>c);
+        System.out.println(a>b || a<c);
+        System.out.println(a<b && a>c);
+	}
+}
