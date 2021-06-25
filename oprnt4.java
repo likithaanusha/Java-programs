2927de5da08b5758504ee728a4f97a5aefbc1efe
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-25 12:35:52 +0530

    Add files via upload

---
+++ oprnt4.java
@@ -0,0 +1,12 @@
+package pack1;
+ public class Oprnt4 {
+    public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = 25;
+		c += a;
+		System.out.println(c);
+	    c -= b;
+		System.out.println(c);
+	}
+}
